20c2feee030300e6bcee61520a41161173adb923
DATBOI <alexsimons9999@gmail.com>
2020-02-22 06:57:07 -0600

    Got most of the linux vscode stuff moved over

---
+++ vscode/extensions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dracula-theme.theme-dracula
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension HookyQR.beautify
+code --install-extension k--kato.intellij-idea-keybindings
+code --install-extension mathiasfrohlich.Kotlin
+code --install-extension ms-python.python
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension unpublished.sourcekit-lsp
+code --install-extension unthrottled.doki-theme
+code --install-extension vscodevim.vim
